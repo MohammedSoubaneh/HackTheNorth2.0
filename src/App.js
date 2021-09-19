70b5454e4b526c091fc8d2bd10132d37d116d134
janetxinli <janetxinli@gmail.com>
2021-09-18 23:18:56 -0700

    Create signup page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Signup from "./pages/Signup";
 import logo from "./logo.svg";
 import "./App.css";
 import HeroImage from "./Images/HeroSushiImage.jpg";
@@ -6,23 +8,33 @@ import BottomLeftImage from "./Images/SushiBottomRight.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <div className="aboutUs">Our Story</div>
-      <div className="headerOne">Sushi</div>
-      <div className="headerTwo">Wave</div>
-      <div className="imageOne">
-        <img src={HeroImage} width="626px" height="400px" />
-      </div>
-      <div className="imageTwo">
-        <img src={BottomRightImage} width="223px" />
-      </div>
-      <div className="imageThree">
-        <img src={BottomLeftImage} width="380px" />
-      </div>
-      <div className="imageThree">
-        <img />
-      </div>
-    </div>
+    <Router>
+      <Route exact path="/">
+        <div className="App">
+          <div className="headerText aboutUs">Our Story</div>
+          <div>
+            <Link to="/signup" className="headerText signup">
+              Sign Up
+            </Link>
+          </div>
+          <div className="headerOne">Sushi</div>
+          <div className="headerTwo">Wave</div>
+          <div className="imageOne">
+            <img src={HeroImage} width="626px" height="400px" />
+          </div>
+          <div className="imageTwo">
+            <img src={BottomRightImage} width="223px" />
+          </div>
+          <div className="imageThree">
+            <img src={BottomLeftImage} width="380px" />
+          </div>
+          <div className="imageThree">
+            <img />
+          </div>
+        </div>
+      </Route>
+      <Route exact path="/signup" component={Signup} />
+    </Router>
   );
 }
 
