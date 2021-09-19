f6bcf2b930cbee825b25c11126b90a65fcb42a88
janetxinli <janetxinli@gmail.com>
2021-09-18 20:54:37 -0700

    Implement firebase auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import HeroImage from './Images/HeroSushiImage.jpg';
-import BottomRightImage from './Images/SushiBottomImage.jpg';
-import BottomLeftImage from './Images/SushiBottomRight.jpg';
+import logo from "./logo.svg";
+import "./App.css";
+import HeroImage from "./Images/HeroSushiImage.jpg";
+import BottomRightImage from "./Images/SushiBottomImage.jpg";
+import BottomLeftImage from "./Images/SushiBottomRight.jpg";
 
 function App() {
   return (
     <div className="App">
-      <div className="aboutUs">
-        Our Story
-      </div>
+      <div className="aboutUs">Our Story</div>
       <div className="headerOne">Sushi</div>
       <div className="headerTwo">Wave</div>
       <div className="imageOne">
-        <img src={HeroImage} width='626px' height='400px'/>
+        <img src={HeroImage} width="626px" height="400px" />
       </div>
       <div className="imageTwo">
-        <img src={BottomRightImage} width='223px'/>
+        <img src={BottomRightImage} width="223px" />
       </div>
       <div className="imageThree">
-        <img src={BottomLeftImage} width='380px' />
+        <img src={BottomLeftImage} width="380px" />
       </div>
       <div className="imageThree">
-        <img/>
+        <img />
       </div>
     </div>
   );
