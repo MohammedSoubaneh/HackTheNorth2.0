857aa822699c22ae352a52a39ab466fae30dd70e
Mohammed Soubaneh <mohammedsoubaneh@Mohammeds-Mini.hitronhub.home>
2021-09-19 04:37:30 -0600

    new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Signup from "./pages/Signup";
 import HeroImage from './Images/HeroSushiImage.jpg';
 import BottomRightImage from './Images/SushiBottomImage.jpg';
 import BottomLeftImage from './Images/SushiBottomRight.jpg';
@@ -16,14 +17,18 @@ function App() {
   };
 
   return (
+  <Route>
+    <Route exact path="/">
     <div className="App">
       <div onClick={transitionVideo} className="aboutUs">
         Our Story
       </div>
       <Modal showBio={showBio} transitionVideo={transitionVideo} setshowBio={setshowBio}/>
+      <Link to="/signup" className="headerText signup">
       <div className='loginButton'>
         <span>Login</span>
       </div>
+      </Link>
       <div className="headerOne">Sushi</div>
       <div className="headerTwo">Wave</div>
       <div className="imageOne">
@@ -39,6 +44,9 @@ function App() {
         <img/>
       </div>
     </div>
+    </Route>
+    <Route exact path="/signup" component={Signup} />
+  </Route>
   );
 }
 
