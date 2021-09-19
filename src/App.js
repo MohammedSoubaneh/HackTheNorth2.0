0fb4cf7668708dba36d8bd90a897e12296afdafc
Mohammed Soubaneh <mohammedsoubaneh@Mohammeds-Mini.hitronhub.home>
2021-09-18 19:00:49 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import HeroImage from './Images/HeroSushiImage.jpg';
+import BottomRightImage from './Images/SushiBottomImage.jpg';
+import BottomLeftImage from './Images/SushiBottomRight.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="aboutUs">
+        Our Story
+      </div>
+      <div className="headerOne">Sushi</div>
+      <div className="headerTwo">Wave</div>
+      <div className="imageOne">
+        <img src={HeroImage} width='626px' height='400px'/>
+      </div>
+      <div className="imageTwo">
+        <img src={BottomRightImage} width='223px'/>
+      </div>
+      <div className="imageThree">
+        <img src={BottomLeftImage} width='380px' />
+      </div>
+      <div className="imageThree">
+        <img/>
+      </div>
     </div>
   );
 }
